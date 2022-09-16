723cba6ea592cd8f89c5018eabd80f6837717e32
André Cavalcante <andedis053@gmail.com>
2022-09-16 16:21:27 -0300

    Atualização 2
    Aprendi a usar o Split no JS, usei a mesma função para as duas funcionalidades.
    Falta a letra C - um select, um multi select, um rádio button e um checkbox.

--- JS/scripts.js
+++ JS/scripts.js
@@ -2,19 +2,24 @@ function contChar() {
 	var nome = document.getElementById('nome').value;
     var numero = document.getElementById('numChar').value;
     var tamanho = document.getElementById('tamanho').id;
-
-    var tamanho = nome.length;
-
-    document.getElementById('numChar').value = tamanho;
+    var numeroPal = document.getElementById('numPal').value;
     
 
+    /*Numero de caracteres*/
+    var tamanho = nome.length;
+    document.getElementById('numChar').value = tamanho;
+
+    /*Numero de palavras */
+    
+    var tamanho = nome.split(" ");
+    console.log(tamanho)
+    document.getElementById('numPal').value = tamanho.length;
+    
+    
+    
+
+    
+
+
 }
 
-function contPal() {
-    var numero = document.getElementById('numPal').value;
-    var palavras;
-
-    console.log(1);
-
-
-}
