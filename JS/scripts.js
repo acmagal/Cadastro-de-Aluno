43798b04c3eb7e9e3e5635d17aa30049ea8534f1
André Cavalcante <andedis053@gmail.com>
2022-09-16 12:02:19 -0300

    Atualização 1
    Consegui fazer ele retornar o numero de letras, falta o numero de palavras.
    O desafio maior está sendo:
    - Chamar duas funções ao clicar em apenas um botão
    - Aprender a usar o separador de palavras do JS.

---
+++ JS/scripts.js
@@ -0,0 +1,20 @@
+function contChar() {
+	var nome = document.getElementById('nome').value;
+    var numero = document.getElementById('numChar').value;
+    var tamanho = document.getElementById('tamanho').id;
+
+    var tamanho = nome.length;
+
+    document.getElementById('numChar').value = tamanho;
+    
+
+}
+
+function contPal() {
+    var numero = document.getElementById('numPal').value;
+    var palavras;
+
+    console.log(1);
+
+
+}
